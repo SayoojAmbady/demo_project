03d0b5a37eb6c662efa990f73db338719fd73534
sayooj <sayoojsunil2255@gmail.com>
2024-01-23 12:53:24 +0530

    MongoDb Connection

---
+++ index.js
@@ -0,0 +1,25 @@
+require ('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+const app = express();
+
+mongoose.connect(`mongodb+srv://sayoojsunil:${process.env.mongodbPassword}@cluster0.cxkqacu.mongodb.net/MyntraDb?retryWrites=true&w=majority`);
+
+ 
+const connection = mongoose.connection;
+connection.once("open", () => 
+    console.log("MongoDb is successfully connected")
+)
+
+app.route("/").get((req,res)=>{
+    res.json("Server is configure")
+});
+
+
+app.listen(port,"0.0.0.0",()=>{console.log("Welcome to port:",port);
+})
+
+
+
+
